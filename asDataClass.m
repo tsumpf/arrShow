32c70ecee90e6355ec6f333d3d31f33df87b8005
Markus Untenberger <muntenb@gwdg.de>
2015-04-28 13:24:29 +0200

    Implemented coilCombine as an option in arrShow used the coilCombine function from the repository which however is very slow.

--- asDataClass.m
+++ asDataClass.m
@@ -231,6 +231,57 @@ classdef asDataClass < handle
             obj.applyDimFun(dim,funPtr);
         end
         
+        function coilCombine(obj,dim)
+            A   = obj.dat;
+            si  = size(A);
+            sel = obj.selection.getValueAsCell;
+            order = 1:length(si);
+            order(dim)  = 3;
+            C           = si(dim);
+            order(3)    = dim;
+            permutsi    = si(order);
+            A = permute(A,order);
+            A = reshape(A, si(1), si(2), si(dim), []);
+            
+            % Get image dimensions and set filter size
+            filtsize    = 7;
+            filter_ones = ones(filtsize);
+            A_out       = zeros([si(1:2), size(A,4)]);
+            
+            for i=1:size(A,4)
+                tmp1    = A(:,:,:,i);
+                ctmp1   = conj(tmp1);
+                tmp2    = zeros(si(1), si(2));
+                Rs = zeros(si(1),si(2),C,C);
+                % Get correlation matrices
+                for kc1=1:C
+                    for kc2=1:C
+                        Rs(:,:,kc1,kc2) = Rs(:,:,kc1,kc2) + filter2(filter_ones, ...
+                            tmp1(:,:,kc1) .* ctmp1(:,:,kc2), 'same');
+                    end
+                end
+                % Compute and apply filter at each voxel
+                for kx=1:si(1)
+                    for ky=1:si(2)
+                        % Change suggested by Mark Bydder
+                        [U,~]   = svd(squeeze(Rs(kx,ky,:,:)));
+                        myfilt  = U(:,1);
+                        tmp2(kx,ky) = myfilt'*squeeze(tmp1(kx,ky,:));
+                    end
+                end
+                A_out(:,:,i) = tmp2;
+            end
+            
+            A_out = reshape(A_out, [permutsi(1), permutsi(2), 1, permutsi(4:end)]);
+            A_out = permute(A_out, order);
+            obj.dat = A_out;
+                        
+            obj.selection.reInit( size(obj.dat), sel);
+            
+            obj.updFig();
+        end
+            
+        
         
         function squeeze(obj)
             % squeezes the data array and updates the selection etc. It
