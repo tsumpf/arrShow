1ec3bb5dc4bdcfeaff61daacd77c4571879bb8bb
Tilman Sumpf <tsumpf@gwdg.de>
2015-06-19 11:34:13 +0200

    Added setColor method to the asMarker class Fixed the set method to delete orphaned markers

--- scripts/asMarkPixel.m
+++ scripts/asMarkPixel.m
@@ -1,19 +1,21 @@
 function asMarkPixel(asObj, pos)
+disp('The asMarkPixel script is obsolete, since you can now simply use asObj.markers.add(pos) instead');
+asObj.markers.add(pos);
 
-ah = asObj.getCurrentAxesHandle;
-
-if isvector(pos)
-    nPos = 1;
-    pos = pos(:);
-else
-    nPos= size(pos,2);
-end
-
-for i = 1 : nPos
-    P = pos(:,i);
-%     rect = rectangle('Parent',ah,'Position',[P(1)-.5, P(2)-.5, 1,1],'Curvature',[0,0],...
-%         'HitTest','off','EdgeColor','red');
-    impoint(ah,P(2),P(1));
-end
+% ah = asObj.getCurrentAxesHandle;
+% 
+% if isvector(pos)
+%     nPos = 1;
+%     pos = pos(:);
+% else
+%     nPos= size(pos,2);
+% end
+% 
+% for i = 1 : nPos
+%     P = pos(:,i);
+% %     rect = rectangle('Parent',ah,'Position',[P(1)-.5, P(2)-.5, 1,1],'Curvature',[0,0],...
+% %         'HitTest','off','EdgeColor','red');
+%     impoint(ah,P(2),P(1));
+% end
 
 end
